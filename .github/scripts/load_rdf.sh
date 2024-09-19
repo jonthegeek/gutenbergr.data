c8f6b8649a964188a4e54383c489cced44a310a0
Jon Harmon <jonthegeek@gmail.com>
2024-09-19 06:37:06 -0500

    Better checks that oxigraph is ready.

--- .github/scripts/load_rdf.sh
+++ .github/scripts/load_rdf.sh
@@ -1,6 +1,6 @@
 load_rdf() {
   RDF_DIR=./pg_rdf_files/cache/epub
-  curl -X POST --data-binary @$RDF_DIR/$1/pg$1.rdf \
+  curl --retry 3 -X POST --data-binary @$RDF_DIR/$1/pg$1.rdf \
     -H 'Content-Type: application/rdf+xml' \
     http://localhost:7878/store?graph=http://gutenberg.org/graph/catalog 2>>error_log.txt
 }
