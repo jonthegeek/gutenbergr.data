8e0fcd8f7628f2bb5729be71b27a00a86c5cfb7d
Jon Harmon <jonthegeek@gmail.com>
2024-09-19 05:33:00 -0500

    Save the correct RSS info.

--- .github/scripts/process_rss.sh
+++ .github/scripts/process_rss.sh
@@ -3,21 +3,29 @@
 url="https://www.gutenberg.org/cache/epub/feeds/today.rss"
 rss_content=$(curl -s "$url")
 
-# Extract publication date, ids, and languages
-pub_date=$(echo "$rss_content" | xmllint --xpath 'string(//rss/channel/pubDate)' - | date -d - +"%Y-%m-%d")
+# Extract publication date of the overall feed.
+pub_date=$(echo "$rss_content" | xmllint --xpath 'string(//rss/channel/pubDate)' - | xargs -I{} date -d "{}" +"%Y-%m-%d")
+
+# Extract IDs from the end of the link.
 ids=$(echo "$rss_content" | xmllint --xpath '//rss/channel/item/link/text()' - | grep -o '[0-9]*$')
+
+# Extract and clean languages from the description.
 languages=$(echo "$rss_content" | xmllint --xpath '//rss/channel/item/description/text()' - | sed 's/^Language: //g' | tr '[:upper:]' '[:lower:]')
 
-# Combine extracted data into a temporary file
-paste <(echo "$pub_date") <(echo "$ids") <(echo "$languages") > updates_temp.csv
+# Make sure we have the same number of IDs and languages.
+if [ $(echo "$ids" | wc -l) -ne $(echo "$languages" | wc -l) ]; then
+    echo "Error: Number of IDs and languages do not match."
+    exit 1
+fi
 
-# Merge the new data with the existing CSV file
+# Create a new CSV with date, id, and language
+paste <(echo "$pub_date") <(echo "$ids") <(echo "$languages") | awk 'BEGIN {FS=OFS="\t"} {print $1, $2, $3}' > updates_temp.csv
+
+# Merge with existing CSV file and sort
 {
     echo "date,id,language"
     cat ./data-raw/rss-updates.csv
     cat updates_temp.csv
-# Use awk to keep only the first occurrence of each id
-} | awk -F, '!seen[$2]++' > ./data-raw/rss-updates.csv
+} | sort -u -k2,2 -t ',' > ./data-raw/rss-updates.csv
 
-# Clean up the temporary file
 rm updates_temp.csv
